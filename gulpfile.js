54563e4c555db48309124efa6e9c758327f8b72e
Yuki Kodama <endflow.net@gmail.com>
2015-06-14 01:15:52 +0900

    Prevent breaking pipe on error in process

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@ var gulp = require('gulp');
 var util = require('gulp-util');
 var cjsx = require('gulp-cjsx');
 var browserify = require('gulp-browserify');
-var watch = require('gulp-watch')
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 gulp.task('transpile', function() {
   gulp.src('src/*.cjsx')
+    .pipe(plumber())
     .pipe(watch('src/*.cjsx'))
-    .pipe(cjsx({bare: true}).on('error', util.log))
+    .pipe(cjsx({bare: true}))
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('browserify', ['transpile'], function() {
+gulp.task('default', ['transpile'], function() {
   gulp.src('build/app.js')
+    .pipe(plumber())
     .pipe(watch('build/*.js'))
     .pipe(browserify({insertGlobals: true}))
     .pipe(gulp.dest('js'))
